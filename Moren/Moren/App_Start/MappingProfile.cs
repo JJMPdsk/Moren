a05807251b18d395fd689a0f93bc314b984ae6a8
Szymon Pituła <deesugfx@gmail.com>
2019-03-26 22:31:36 +0100

    Added API for movies & customers

---
+++ Moren/Moren/App_Start/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Moren.Dtos;
+using Moren.Models;
+
+namespace Moren.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
+
+        }
+    }
+}
