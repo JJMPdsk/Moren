06e3b4c8c3b4b080291ef4697e7c8fedb3e390d6
Szymon Pituła <deesugfx@gmail.com>
2019-03-26 17:53:48 +0100

    Added validation for movie/customer forms

--- Moren/Moren/Controllers/MoviesController.cs
+++ Moren/Moren/Controllers/MoviesController.cs
@@ -52,9 +52,8 @@ namespace Moren.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
 
@@ -62,8 +61,18 @@ namespace Moren.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
--- Moren/Moren/Models/MembershipType.cs
+++ Moren/Moren/Models/MembershipType.cs
@@ -16,5 +16,12 @@ namespace Moren.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        public static readonly byte Unknown = 0;
+        public static readonly byte PayAsYouGo = 1;
+        public static readonly byte Monthly = 2;
+        public static readonly byte Quarterly = 3;
+        public static readonly byte Annual = 4;
+
     }
 }
